16bed22db608e9b4faaf6f586e679601ba284a6b
Youbess <alexexocet@gmail.com>
2022-02-08 14:11:21 +0100

    add turn functions

--- projet.js
+++ projet.js
@@ -17,6 +17,42 @@ let rover = {
 
 const turnLeft = (roverLeft) => {
 
-    console.log(roverLeft.direction)
-    
+    switch (roverLeft.direction) {
+		case 'N':
+			roverLeft.direction = 'W';
+			break
+		case 'W':
+			roverLeft.direction = 'S';
+			break
+		case 'S':
+			roverLeft.direction = 'E';
+			break
+		case 'E':
+			roverLeft.direction = 'N';
+			break
+		default:
+			roverLeft.direction = 'N';
+			break
+	}
+};
+
+const turnRight = (roverRight) => {
+
+    switch (roverRight.direction) {
+		case 'N':
+			roverRight.direction = 'E';
+			break
+		case 'W':
+			roverRight.direction = 'N';
+			break
+		case 'S':
+			roverRight.direction = 'W';
+			break
+		case 'E':
+			roverRight.direction = 'S';
+			break
+		default:
+			roverRight.direction = 'N';
+			break	
+	}
 };
